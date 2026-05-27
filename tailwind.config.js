c53b664605c7554ce14e06d5c65f047835a87521
NethmiDhananjana1011 <nethnethmidhananjana1011@gmail.com>
2026-05-27 12:11:36 +0530

    finalize login logic and localstorage session management

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Check if this line is exactly like this
   ],
   theme: {
     extend: {},
